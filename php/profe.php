f4fc9a35015bcea5f496ebed33cbf0aeda82d489
Gagandeep Singh <122905398+gagansingh9@users.noreply.github.com>
2023-05-03 20:02:11 +0200

    Update profe.php

--- php/profe.php
+++ php/profe.php
@@ -17,7 +17,7 @@
   <nav class="navbar sticy-top navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
       <a class="navbar-brand" href="../php/Alumno.php">
-        <img src="logo.png" id="logo">
+        <img src="../logo.png" id="logo">
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
