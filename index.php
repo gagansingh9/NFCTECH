50ce00b0b2ad210e2f40741328c050546f64209b
Gagan <gmunder9@gmail.com>
2023-05-03 20:35:35 +0200

    03/05

--- index.php
+++ index.php
@@ -26,6 +26,9 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="../app/PagPromo/quienessomos.html ">Sobre Nosotros</a>
+        </li>
                      <li class="nav-item " id="registro">
                      <form action="php/Registrate.php">
                        <input class="btn btn-secondary" type="submit" value="Regístrate" />
